c1313958496fccc65c1be45dae8b78a89f5d2a6c
Daniel Larsen <daniel@larsen.net.nz>
2014-05-24 13:07:03 +1200

    First commit

---
+++ server.js
@@ -0,0 +1,43 @@
+﻿var http = require('http');
+var https = require('https');
+var port = process.env.port || 1337;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    // request API data from NZTA
+    //https://infoconnect1.highwayinfo.govt.nz/ic/jbi/SsdfJourney2/REST/FeedService/journey/R04-NB
+
+    var nztaOptions = {
+        host: 'infoconnect1.highwayinfo.govt.nz',
+        port: 443,
+        path: '/ic/jbi/SsdfJourney2/REST/FeedService/journey/R04-NB',
+        headers: {
+            "username": "DanielLa",
+            "password": "Password1"
+        }
+    };
+
+    https.get(nztaOptions, function (nztaResponse) {
+        var nztaData = '';
+
+        nztaResponse.on('data', function (chunk) {
+            // chunk response from NZTA
+            nztaData += chunk;
+            var message = "" + chunk; 
+            console.log('Received response of ' + message.length + ' bytes from nzta.');
+        });
+
+        nztaResponse.on('end', function () {
+            // final response, now process data
+            console.log('End response.');
+
+            // end response
+            res.statusCode = 200;
+            res.end(nztaData);
+        });
+
+    });
+
+    
+}).listen(port);
