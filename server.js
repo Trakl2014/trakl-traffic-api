a11ea58e8c88cd36a9a96ea2835c6f3645c247a8
Daniel Larsen <daniel@larsen.net.nz>
2014-05-24 15:07:53 +1200

    now returns live realtime average speed for journey

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ var xml2js = require('xml2js');
 
 var port = process.env.port || 1337;
 
-
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'application/json' });
 
     // request API data from NZTA
     //https://infoconnect1.highwayinfo.govt.nz/ic/jbi/SsdfJourney2/REST/FeedService/journey/R04-NB
@@ -36,22 +35,18 @@ http.createServer(function (req, res) {
             console.log('response end.');
 
             xml2js.parseString(nztaData, function (err, result) {
-                
-                var nztaJson = JSON.stringify(result);
+                var journey = {
+                    name: result["tns:findJourneyByReferenceResponse"]["tns:return"][0]["tns:name"][0],
+                    averageSpeed: result["tns:findJourneyByReferenceResponse"]["tns:return"][0]["tns:averageSpeed"][0]
+                }
 
                 // end response
                 res.statusCode = 200;
-                res.end(nztaJson);
-
-                
+                res.end(JSON.stringify(journey));
             });
-
-
-
 
         });
 
     });
 
-    
 }).listen(port);
