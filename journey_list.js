14dc1782a72db995990fd2e5b0966d5a42465442
Peter Rosser <tharax@gmail.com>
2014-05-24 20:50:29 +1200

    Added Start & End Co-ords to Journeys

--- journey_list.js
+++ journey_list.js
@@ -3,18 +3,21 @@ exports.getNames = function (filename, callback) {
 
 	fs.readFile(filename, function (err,data) {
 		var parseString = require('xml2js').parseString;
-		// console.log("data:" + data);
-		console.log("I've got the data");
+		console.log("I've got the data (from the XML file)");
 		parseString(data, function (err, result) {
 
 			var journeys = [];
 			for (var i = 0; i < result["tns:findAllJourneysResponse"]["tns:return"].length; i++) {
-				console.log("l:" +result["tns:findAllJourneysResponse"]["tns:return"].length + "i:"+ i);
+				// console.log("length: " +result["tns:findAllJourneysResponse"]["tns:return"].length + " i: "+ i);
 
 				if (!result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:name"]) continue;
 				journey = {
 					name: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:name"][0],
 					ref: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:reference"][0],
+					startLat: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"][0]["tns:startLocation"][0]["tns:latitude"],
+					startLong: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"][0]["tns:startLocation"][0]["tns:longitude"],
+					endLat: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"][result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"].length - 1]["tns:endLocation"][0]["tns:latitude"],
+					endLong: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"][result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"].length - 1]["tns:endLocation"][0]["tns:longitude"]
 				}
 				journeys[i] = journey;
 			}
