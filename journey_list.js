0bbe3e34cc6d0a70a7a877243babdab5594fa408
Peter Rosser <tharax@gmail.com>
2014-05-25 13:33:46 +1200

    dropped motorway supports

--- journey_list.js
+++ journey_list.js
@@ -9,19 +9,19 @@ exports.getNames = function (filename, callback) {
 			var journeys = [];
 			for (var i = 0; i < result["tns:findAllJourneysResponse"]["tns:return"].length; i++) {
 				// console.log("length: " +result["tns:findAllJourneysResponse"]["tns:return"].length + " i: "+ i);
-
-				if (!result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:name"]) continue;
-				journey = {
-					name: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:name"][0],
-					ref: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:reference"][0],
-					startLat: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"][0]["tns:startLocation"][0]["tns:latitude"][0],
-					startLong: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"][0]["tns:startLocation"][0]["tns:longitude"][0],
-					endLat: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"][result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"].length - 1]["tns:endLocation"][0]["tns:latitude"][0],
-					endLong: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"][result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"].length - 1]["tns:endLocation"][0]["tns:longitude"][0]
+				if (result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:type"][0]["tns:id"][0] = "3") {
+					if (!result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:name"]) continue;
+					journey = {
+						name: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:name"][0],
+						ref: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:reference"][0],
+						startLat: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"][0]["tns:startLocation"][0]["tns:latitude"][0],
+						startLong: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"][0]["tns:startLocation"][0]["tns:longitude"][0],
+						endLat: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"][result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"].length - 1]["tns:endLocation"][0]["tns:latitude"][0],
+						endLong: result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"][result["tns:findAllJourneysResponse"]["tns:return"][i]["tns:segments"].length - 1]["tns:endLocation"][0]["tns:longitude"][0]
+					}
+					journeys[i] = journey;
 				}
-				journeys[i] = journey;
 			}
-
 			callback(JSON.stringify(journeys));
 		});
 	});
