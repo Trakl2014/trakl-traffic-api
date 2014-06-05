67688a264bc58dfe87e7ffb92899d1972507abf2
Lee <leeblazek@gmail.com>
2014-06-05 15:24:38 +1200

    got basic journey ref going
    not saving anywhere tho

--- journey.js
+++ journey.js
@@ -1,4 +1,4 @@
-﻿exports.getJourneyData = function (reqUrl, callback) {
+﻿exports.getJourneyData = function(reqUrl, callback) {
     var https = require('https');
     var xml2js = require('xml2js');
     var url = require('url');
@@ -7,7 +7,7 @@
     var uuid = require('node-uuid');
 
     var secondsBetweenPolls = 600;
-    
+
     // request API data from NZTA
     //https://infoconnect1.highwayinfo.govt.nz/ic/jbi/SsdfJourney2/REST/FeedService/journey/R04-NB
 
@@ -24,133 +24,136 @@
         port: 443,
         path: '/ic/jbi/SsdfJourney2/REST/FeedService/journey/' + ref,
         headers: {
-            "username": process.env.NztaUsername,
-            "password": process.env.NztaPassword
+            "username": "LeeB",
+            "password": "H9r8z6w3g9"
         }
     };
 
-    https.get(nztaOptions, function (nztaResponse) {
+    https.get(nztaOptions, function(nztaResponse) {
         var nztaData = '';
 
-        nztaResponse.on('data', function (chunk) {
+        nztaResponse.on('data', function(chunk) {
             // chunk response from NZTA
             nztaData += chunk;
             var message = "" + chunk;
             console.log('Received response of ' + message.length + ' bytes from nzta.');
         });
 
-        nztaResponse.on('end', function () {
+        nztaResponse.on('end', function() {
             // final response, now process data
             console.log('response end.');
 
-            xml2js.parseString(nztaData, function (err, result) {
+            xml2js.parseString(nztaData, function(err, result) {
+                console.log(nztaData);
+                console.log('--------');
+                console.log(result["tns:findJourneyByReferenceResponse"]["tns:return"][0]["tns:name"][0]);
 
-                var tableService = azure.createTableService(process.env.StorageAccountName,
-                    process.env.StorageAccountKey,
-                    process.env.StorageAccountTableStoreHost);
+                //     var tableService = azure.createTableService(process.env.StorageAccountName,
+                //         process.env.StorageAccountKey,
+                //         process.env.StorageAccountTableStoreHost);
 
-                tableService.createTableIfNotExists('journey', function (error) {
-                    if (error) {
-                        throw 'Could not create table: ' + error;
-                    }
+                //     tableService.createTableIfNotExists('journey', function (error) {
+                //         if (error) {
+                //             throw 'Could not create table: ' + error;
+                //         }
 
-                    var query = azure.TableQuery
-                        .select()
-                        .from('journey')
-                        .where('PartitionKey eq ?', ref);
+                //         var query = azure.TableQuery
+                //             .select()
+                //             .from('journey')
+                //             .where('PartitionKey eq ?', ref);
 
-                    tableService.queryEntities(query, function (queryError, entities) {
-                        if (queryError) {
-                            throw 'Could not query entities: ' + queryError;
-                        }
+                //         tableService.queryEntities(query, function (queryError, entities) {
+                //             if (queryError) {
+                //                 throw 'Could not query entities: ' + queryError;
+                //             }
 
-                        var lastJourney;
+                //             var lastJourney;
 
-                        if (entities.length == 0) {
-                            lastJourney = {
-                                PartitionKey: ref,
-                                RowKey: uuid.v4(),
-                                averageSpeed: "",
-                                minutes: "",
-                                pollDateTime: ""
-                            };
-                        } else {
-                            // get any journeys older than secondsBetweenPolls and delete all but the earliest one
-                            var lastPollDate = new Date(result["tns:findJourneyByReferenceResponse"]["tns:return"][0]["tns:lastEstimateTime"][0]);
-                            lastPollDate.setSeconds(lastPollDate.getSeconds() - secondsBetweenPolls);
+                //             if (entities.length == 0) {
+                //                 lastJourney = {
+                //                     PartitionKey: ref,
+                //                     RowKey: uuid.v4(),
+                //                     averageSpeed: "",
+                //                     minutes: "",
+                //                     pollDateTime: ""
+                //                 };
+                //             } else {
+                //                 // get any journeys older than secondsBetweenPolls and delete all but the earliest one
+                //                 var lastPollDate = new Date(result["tns:findJourneyByReferenceResponse"]["tns:return"][0]["tns:lastEstimateTime"][0]);
+                //                 lastPollDate.setSeconds(lastPollDate.getSeconds() - secondsBetweenPolls);
 
-                            var minPollDate;
-                            var oldJourneys = [];
-                            var j = 0;
-                            for (var i = 0; i < entities.length; i++) {
-                                if (new Date(entities[i].pollDateTime) <= lastPollDate) {
-                                    if (minPollDate === undefined || new Date(entities[i].pollDateTime) > minPollDate) {
-                                        minPollDate = new Date(entities[i].pollDateTime);
-                                    }
-                                    oldJourneys[j] = entities[i];
-                                    j++;
-                                }
-                            }
+                //                 var minPollDate;
+                //                 var oldJourneys = [];
+                //                 var j = 0;
+                //                 for (var i = 0; i < entities.length; i++) {
+                //                     if (new Date(entities[i].pollDateTime) <= lastPollDate) {
+                //                         if (minPollDate === undefined || new Date(entities[i].pollDateTime) > minPollDate) {
+                //                             minPollDate = new Date(entities[i].pollDateTime);
+                //                         }
+                //                         oldJourneys[j] = entities[i];
+                //                         j++;
+                //                     }
+                //                 }
 
-                            // delete all but earliest >= lastPollDate journey 
-                            for (i = 0; i < oldJourneys.length; i++) {
-                                if (oldJourneys[i].pollDateTime == minPollDate) continue;
-                                
-                                // delete
-                                tableService.deleteEntity('journey', {
-                                    PartitionKey: oldJourneys[i].PartitionKey,
-                                    RowKey: oldJourneys[i].RowKey
-                                }, function (deleteError) {
-                                    if (deleteError) {
-                                        throw deleteError;
-                                    }
-                                });
-                            }
+                //                 // delete all but earliest >= lastPollDate journey 
+                //                 for (i = 0; i < oldJourneys.length; i++) {
+                //                     if (oldJourneys[i].pollDateTime == minPollDate) continue;
 
-                            // find the oldest journey
-                            var maxPollDate;
-                            j = 0;
-                            for (i = 0; i < entities.length; i++) {
-                                if (!maxPollDate) {
-                                    maxPollDate = new Date(entities[i].pollDateTime);
-                                    j = i;
-                                    continue;
-                                }
-                                if (new Date(entities[i].pollDateTime) < maxPollDate) {
-                                    maxPollDate = new Date(entities[i].pollDateTime);
-                                    j = i;
-                                }
-                            }
+                //                     // delete
+                //                     tableService.deleteEntity('journey', {
+                //                         PartitionKey: oldJourneys[i].PartitionKey,
+                //                         RowKey: oldJourneys[i].RowKey
+                //                     }, function (deleteError) {
+                //                         if (deleteError) {
+                //                             throw deleteError;
+                //                         }
+                //                     });
+                //                 }
 
-                            lastJourney = entities[j];
-                        }
+                //                 // find the oldest journey
+                //                 var maxPollDate;
+                //                 j = 0;
+                //                 for (i = 0; i < entities.length; i++) {
+                //                     if (!maxPollDate) {
+                //                         maxPollDate = new Date(entities[i].pollDateTime);
+                //                         j = i;
+                //                         continue;
+                //                     }
+                //                     if (new Date(entities[i].pollDateTime) < maxPollDate) {
+                //                         maxPollDate = new Date(entities[i].pollDateTime);
+                //                         j = i;
+                //                     }
+                //                 }
 
-                        var journey = {
-                            PartitionKey: ref,
-                            RowKey: uuid.v4(),
+                //                 lastJourney = entities[j];
+                //             }
 
-                            name: result["tns:findJourneyByReferenceResponse"]["tns:return"][0]["tns:name"][0],
+                var journey = {
+                    // PartitionKey: ref,
+                    // RowKey: uuid.v4(),
 
-                            averageSpeed: result["tns:findJourneyByReferenceResponse"]["tns:return"][0]["tns:averageSpeed"][0],
-                            minutes: result["tns:findJourneyByReferenceResponse"]["tns:return"][0]["tns:lastEstimate"][0],
-                            pollDateTime: result["tns:findJourneyByReferenceResponse"]["tns:return"][0]["tns:lastEstimateTime"][0],
+                    name: result["tns:findJourneyByReferenceResponse"]["tns:return"][0]["tns:name"][0],
+                    averageSpeed: result["tns:findJourneyByReferenceResponse"]["tns:return"][0]["tns:averageSpeed"][0],
+                    minutes: result["tns:findJourneyByReferenceResponse"]["tns:return"][0]["tns:lastEstimate"][0],
+                    pollDateTime: result["tns:findJourneyByReferenceResponse"]["tns:return"][0]["tns:lastEstimateTime"][0]
+                    // lastAverageSpeed: lastJourney.averageSpeed,
+                    // lastMinutes: lastJourney.minutes,
+                    // lastPollDateTime: lastJourney.pollDateTime
+                };
+                console.log(journey);
+                callback(journey);
 
-                            lastAverageSpeed: lastJourney.averageSpeed,
-                            lastMinutes: lastJourney.minutes,
-                            lastPollDateTime: lastJourney.pollDateTime
-                        };
+                //             // save latest journey
+                //             tableService.insertEntity('journey', journey, function (insertError) {
+                //                 if (insertError) {
+                //                     throw 'Could not insert:' + insertError;
+                //                 }
 
-                        // save latest journey
-                        tableService.insertEntity('journey', journey, function (insertError) {
-                            if (insertError) {
-                                throw 'Could not insert:' + insertError;
-                            }
-
-                            // Entity inserted
-                            callback(journey);
-                        });
-                    });
-                });
+                //                 // Entity inserted
+                //                 callback(journey);
+                //             });
+                //         });
+                //     });
             });
         });
     });
